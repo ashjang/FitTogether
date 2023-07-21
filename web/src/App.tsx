4cb0583bb4687c3b7ce0c48904996700e7a3f42e
서동경 <ehhdrud@icloud.com>
2023-07-22 00:31:59 +0900

    feat: [feature/LogIn] 기본요소구성

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,7 +1,12 @@
 import "./App.css";
+import LogIn from "./pages/LogIn/LogIn";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <LogIn />
+    </>
+  );
 }
 
 export default App;
--- web/src/pages/LogIn/LogIn.tsx
+++ web/src/pages/LogIn/LogIn.tsx
@@ -3,7 +3,31 @@ import React from "react";
 interface Props {}
 
 const LogIn: React.FC<Props> = () => {
-  return <></>;
+  return (
+    <div>
+      <div>카카오톡 로그인</div>
+      <p>또는</p>
+      <form>
+        <div>
+          <input
+            type="text"
+            id="username"
+            name="username"
+            placeholder="이메일 주소를 입력하세요"
+          />
+        </div>
+        <div>
+          <input
+            type="password"
+            id="password"
+            name="password"
+            placeholder="비밀번호를 입력하세요"
+          />
+        </div>
+        <button type="submit">로그인</button>
+      </form>
+    </div>
+  );
 };
 
 export default LogIn;
