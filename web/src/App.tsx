0932357f3ae0d9e381c30bf11dd4bf784065efc8
YoonHyeJin <emfkdlvnem@gmail.com>
2023-07-22 14:30:45 +0900

    feat:[feature/Header]-header mark up start

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,9 +1,34 @@
-import "./App.css";
+import React from 'react';
+
+import { Route, Routes } from 'react-router-dom';
+
+import Header from './components/common/Header';
+
+
 function App() {
 	return (
-		<>
-		</>
-	)
+		<div className="app">
+			<div className="header-body">
+				<Header />
+			</div>
+			<div className="headerUnder">
+				<Routes>
+					<Route path="/" element={<MainPage />} />
+					<Route path="/signup" element={<Signup />} />
+					<Route path="/login" element={<Login />} />
+					<Route path="/festivalProduct" element={<FestivalProduct />} />
+					<Route path="/festivalDetail/:id" element={<FestivalDetail />} />
+					<Route path="/oitaProduct" element={<OitaProduct />} />
+					<Route path="/oitaDetail/:id" element={<OitaDetail />} />
+					<Route path="/cart" element={<Cart />} />
+					<Route path="/wishList" element={<WishList />} />
+				</Routes>
+			</div>
+			<div className="footer-body">
+				{/* <Footer /> */}
+			</div>
+		</div> 
+	);
 }
 
 export default App;
--- web/src/components/common/Header.tsx
+++ web/src/components/common/Header.tsx
@@ -1,5 +1,3 @@
-import React from 'react';
-
 function Header() {
     return (
         <div className="header-inn">
