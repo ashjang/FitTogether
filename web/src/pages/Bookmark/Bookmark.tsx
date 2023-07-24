73f674091ef489d975d7d802a4bac221caddeb57
DESKTOP-UPBTJS0\yangs <yangsh9161@gmail.com>
2023-07-25 02:50:48 +0900

    즐겨찾기 페이지 레이아웃 1차 수정 및 '+'버튼 눌렀을때 뜨는 즐겨찾기 팝업 세팅

--- web/src/pages/Bookmark/Bookmark.tsx
+++ web/src/pages/Bookmark/Bookmark.tsx
@@ -4,20 +4,16 @@ import React, { useState } from 'react';
 
 import styled from '@emotion/styled';
 import { css } from '@emotion/react';
-import { FaPlus } from 'react-icons/fa';
+import { FaPlus, FaMinus } from 'react-icons/fa';
 
 import BookmarkFolder from '../../components/Bookmark/BookmarkFolder';
 import BookmarkSetting from '../../components/Bookmark/BookmarkSetting';
 
 const Bookmark: React.FC = () => {
-  const [isPopupOpen, setIsPopupOpen] = useState(false);
+  const [isPopupOpen, setIsPopupOpen] = useState<boolean>(false);
 
-  const openPopup = () => {
-    setIsPopupOpen(true);
-  };
-
-  const closePopup = () => {
-    setIsPopupOpen(false);
+  const togglePopup = () => {
+    setIsPopupOpen((prevIsPopupOpen) => !prevIsPopupOpen);
   };
 
   return (
@@ -26,12 +22,13 @@ const Bookmark: React.FC = () => {
       <div css={Container}>
         <TitleArea>
           <p css={centeredTextStyle}>즐겨찾기</p>
-          <FaPlus css={[rightAlignedStyle, plusIcon]} onClick={openPopup} />
-          {isPopupOpen && (
-            <div css={popupStyle}>
+          {isPopupOpen ? (
+            <>
               <BookmarkSetting />
-              <button onClick={closePopup}>닫기</button>
-            </div>
+              <FaMinus css={rightAlignedStyle} onClick={togglePopup} />
+            </>
+          ) : (
+            <FaPlus css={rightAlignedStyle} onClick={togglePopup} />
           )}
         </TitleArea>
       </div>
@@ -63,27 +60,14 @@ const TitleArea = styled.div`
 const centeredTextStyle = css`
   flex: 25;
   text-align: center;
+  font-size: 26px;
+  font-weight: 700;
 `;
 
 const rightAlignedStyle = css`
   flex: 1;
   text-align: right;
-`;
-
-const plusIcon = css`
   cursor: pointer;
-`;
-
-const popupStyle = css`
-  position: fixed;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-  background-color: #fff;
-  padding: 20px;
-  border: solid 1px black;
-  border-radius: 20px;
-  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.4);
 `;
 
 export default Bookmark;
