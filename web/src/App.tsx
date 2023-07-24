d145cc67d989537d1592a380a708261bcbfccf1a
DESKTOP-UPBTJS0\yangs <yangsh9161@gmail.com>
2023-07-24 16:23:35 +0900

    즐겨찾기 페이지 레이아웃

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,7 +1,12 @@
-import "./App.css";
+import './App.css';
+import Bookmark from './pages/Bookmark/Bookmark';
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <Bookmark />
+    </>
+  );
 }
 
 export default App;
---
+++ web/src/components/Bookmark_MyVideos/MyVideoList.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const MyVideoList: React.FC = () => {
+  return <div>개인이 저장한 비디오들</div>;
+};
+
+export default MyVideoList;
