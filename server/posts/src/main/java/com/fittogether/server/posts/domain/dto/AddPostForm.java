f7c33ffa923276999fa4138cbfd200ab1190ab75
woojkk <122269418+woojkk@users.noreply.github.com>
2023-07-25 17:05:18 +0900

    Update AddPostForm.java

--- server/posts/src/main/java/com/fittogether/server/posts/domain/dto/AddPostForm.java
+++ server/posts/src/main/java/com/fittogether/server/posts/domain/dto/AddPostForm.java
@@ -2,8 +2,7 @@ package com.fittogether.server.posts.domain.dto;
 
 import com.fittogether.server.posts.domain.model.Hashtag;
 import com.fittogether.server.user.type.Category;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.Size;
+import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -17,12 +16,10 @@ import lombok.Setter;
 @Builder
 public class AddPostForm {
 
-  @NotBlank(message = "제목은 필수 입력 사항입니다.")
   private String title;
-  @NotBlank(message = "내용은 필수 입력 사항입니다.")
   private String description;
   private String image;
   private Category category;
   private boolean accessLevel;
-  private Hashtag hashtag;
+  private List<Hashtag> hashtag;
 }
