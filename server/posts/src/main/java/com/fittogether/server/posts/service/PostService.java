0ae1ee4948c630f788de6019b719791e7e1df361
woojkk <122269418+woojkk@users.noreply.github.com>
2023-07-25 17:02:07 +0900

    Update PostService.java
    해시태그 여러개 받을 수 있도록 변경

--- server/posts/src/main/java/com/fittogether/server/posts/service/PostService.java
+++ server/posts/src/main/java/com/fittogether/server/posts/service/PostService.java
@@ -9,6 +9,8 @@ import com.fittogether.server.posts.domain.repository.HashtagRepository;
 import com.fittogether.server.posts.domain.repository.PostHashtagRepository;
 import com.fittogether.server.posts.domain.repository.PostRepository;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -31,17 +33,20 @@ public class PostService {
 
 //    if (!addPostForm.isAccessLevel()) {
 //    }
+    
+    List<Hashtag> hashtag = addPostForm.getHashtag();
+    List<Hashtag> savedHashtag = new ArrayList<>();
 
-    Hashtag hashtag = addPostForm.getHashtag();
 
+    for (Hashtag hashtags : hashtag) {
     Hashtag keyword = hashtagRepository.findByKeyword(hashtag.getKeyword());
 
     if (keyword == null) {
       // 해당 해시태그가 데이터베이스에 존재하지 않으면, 새로운 해시태그를 저장합니다.
-      hashtag = hashtagRepository.save(hashtag);
+      savedHashtag.add(hashtagRepository.save(hashtags));
     } else {
       // 해당 해시태그가 이미 존재한다면, 기존의 것을 사용합니다.
-      hashtag = keyword;
+      savedHashtag.add(keyword);
     }
 
     Post post = Post.builder()
@@ -52,11 +57,13 @@ public class PostService {
         .accessLevel(addPostForm.isAccessLevel())
         .createdAt(LocalDateTime.now())
         .build();
-
+      
+    for (Hashtag hashtags : savedHashtag) {
     postHashtagRepository.save(PostHashtag.builder()
         .post(post)
         .hashtag(hashtag)
         .build());
+    }
 
     return postRepository.save(post);
   }
