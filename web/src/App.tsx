7227c3a885f341152c47e0c3ed5d4ec06ac2839d
DESKTOP-UPBTJS0\yangs <yangsh9161@gmail.com>
2023-07-27 03:02:28 +0900

    feat: Bookmark 및 BookmarkSetting 1차 마크업

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,22 +1,24 @@
-import { BrowserRouter ,Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 import Header from './components/common/Header';
+import Bookmark from './pages/Bookmark/Bookmark';
 // import Footer from './components/common/Footer';
 
 function App() {
-	return (
-        <BrowserRouter>
-            <Header />
-                <Routes>
-                    {/* <Route path='/' element={<Index />} /> */}
-                    <Route path='/header' element={<Header />} />
-                    {/* <Route path='/footer' element={<Footer />} />
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        {/* <Route path='/' element={<Index />} /> */}
+        <Route path="/header" element={<Header />} />
+        {/* <Route path='/footer' element={<Footer />} />
                     <Route path='/login' element={<Login />} />
                     <Route path='/signin' element={<Signin />} /> */}
-                </Routes>
-            {/* <Footer /> */}
-        </BrowserRouter>
-	);
+      </Routes>
+      {/* <Footer /> */}
+      <Bookmark />
+    </BrowserRouter>
+  );
 }
 
 export default App;
--- web/src/pages/Bookmark/Bookmark.tsx
+++ web/src/pages/Bookmark/Bookmark.tsx
@@ -4,7 +4,7 @@ import React, { useState } from 'react';
 
 import styled from '@emotion/styled';
 import { css } from '@emotion/react';
-import { FaPlus } from 'react-icons/fa';
+import { FaPlus, FaMinus } from 'react-icons/fa';
 
 import BookmarkFolder from '../../components/Bookmark/BookmarkFolder';
 import BookmarkSetting from '../../components/Bookmark/BookmarkSetting';
@@ -12,12 +12,8 @@ import BookmarkSetting from '../../components/Bookmark/BookmarkSetting';
 const Bookmark: React.FC = () => {
   const [isPopupOpen, setIsPopupOpen] = useState(false);
 
-  const openPopup = () => {
-    setIsPopupOpen(true);
-  };
-
-  const closePopup = () => {
-    setIsPopupOpen(false);
+  const togglePopup = () => {
+    setIsPopupOpen((prevIsPopupOpen) => !prevIsPopupOpen);
   };
 
   return (
@@ -26,12 +22,13 @@ const Bookmark: React.FC = () => {
       <div css={Container}>
         <TitleArea>
           <p css={centeredTextStyle}>즐겨찾기</p>
-          <FaPlus css={[rightAlignedStyle, plusIcon]} onClick={openPopup} />
-          {isPopupOpen && (
-            <div css={popupStyle}>
+          {isPopupOpen ? (
+            <>
               <BookmarkSetting />
-              <button onClick={closePopup}>닫기</button>
-            </div>
+              <FaMinus css={[rightAlignedStyle, icon]} onClick={togglePopup} />
+            </>
+          ) : (
+            <FaPlus css={[rightAlignedStyle, icon]} onClick={togglePopup} />
           )}
         </TitleArea>
       </div>
@@ -70,20 +67,8 @@ const rightAlignedStyle = css`
   text-align: right;
 `;
 
-const plusIcon = css`
+const icon = css`
   cursor: pointer;
-`;
-
-const popupStyle = css`
-  position: fixed;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-  background-color: #fff;
-  padding: 20px;
-  border: solid 1px black;
-  border-radius: 20px;
-  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.4);
 `;
 
 export default Bookmark;
