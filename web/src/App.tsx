b306b0068cbfd77c369ff059d3b4be204d515382
ehhdrud <106059716+ehhdrud@users.noreply.github.com>
2023-07-26 19:07:07 +0900

    Revert "Feature/log in"

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,22 +1,7 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-
-import Header from "./components/common/Header";
-// import Footer from './components/common/Footer';
+import "./App.css";
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Header />
-      <Routes>
-        {/* <Route path='/' element={<Index />} /> */}
-        <Route path="/header" element={<Header />} />
-        {/* <Route path='/footer' element={<Footer />} />
-                    <Route path='/login' element={<Login />} />
-                    <Route path='/signin' element={<Signin />} /> */}
-      </Routes>
-      {/* <Footer /> */}
-    </BrowserRouter>
-  );
+  return <></>;
 }
 
 export default App;
