df371b98ea4ebc6576df3d4b465bffb2e2710127
DESKTOP-UPBTJS0\yangs <yangsh9161@gmail.com>
2023-07-26 17:11:23 +0900

    머지 전 현재 변경 사항 커밋

--- web/src/components/Community_Post/Comments.tsx
+++ web/src/components/Community_Post/Comments.tsx
@@ -1,88 +1,101 @@
-import React, { useState } from 'react';
+/** @jsxImportSource @emotion/react */
 
+import React, { useState } from 'react';
+import styled from '@emotion/styled';
 interface Comment {
   id: number;
   content: string;
+  likes: number;
+  dislikes: number;
   replies: Comment[];
 }
 
-interface CommentsProps {
-  comments: Comment[];
-}
+const Comments: React.FC = () => {
+  const [comments, setComments] = useState<Comment[]>([]);
+  const [newComment, setNewComment] = useState('');
 
-const Comments: React.FC<CommentsProps> = ({ comments }) => {
-  const [newComment, setNewComment] = useState<string>('');
-
-  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setNewComment(event.target.value);
-  };
-
-  const handleAddComment = (parentId: number | null) => {
+  const handleAddComment = () => {
     if (newComment.trim() !== '') {
       const newCommentObject: Comment = {
-        id: Date.now(), // 간단한 예시를 위해 일시적으로 현재 시간을 id로 사용합니다.
+        id: Date.now(), // 간단한 예시를 위해 일시적으로 현재 시간을 id로 사용
         content: newComment,
+        likes: 0,
+        dislikes: 0,
         replies: [],
       };
-
-      if (parentId !== null) {
-        // 대댓글인 경우
-        const parentComment = findComment(comments, parentId);
-        if (parentComment) {
-          parentComment.replies.push(newCommentObject);
-        }
-      } else {
-        // 댓글인 경우
-        comments.push(newCommentObject);
-      }
-
+      setComments([...comments, newCommentObject]);
       setNewComment('');
     }
   };
 
-  const findComment = (
-    comments: Comment[],
-    id: number
-  ): Comment | undefined => {
-    for (const comment of comments) {
-      if (comment.id === id) {
-        return comment;
-      }
-      const reply = findComment(comment.replies, id);
-      if (reply) {
-        return reply;
-      }
-    }
-    return undefined;
-  };
-
   return (
-    <>
-      {/* 댓글 입력창 */}
-      <div>
-        <input type="text" value={newComment} onChange={handleInputChange} />
-        <button onClick={() => handleAddComment(null)}>입력</button>
-      </div>
+    <CommentContainer>
+      <NewCommentInput>
+        {/* 댓글 입력창 */}
+        <img src="http://placehold.it/60x60" alt="프로필 이미지" />
+        <input
+          type="text"
+          value={newComment}
+          onChange={(e) => setNewComment(e.target.value)}
+          placeholder="댓글을 입력하세요"
+        />
+      </NewCommentInput>
+      <NewCommentArea>
+        <button onClick={handleAddComment}>취소</button>
+        <button onClick={handleAddComment}>댓글</button>
+      </NewCommentArea>
 
       {/* 댓글 목록 */}
+      <div></div>
       {comments.map((comment) => (
         <div key={comment.id}>
+          <img src="http://placehold.it/60x60" alt="프로필 이미지" />
+          <p>{comment.id}</p>
           <p>{comment.content}</p>
-          <div style={{ marginLeft: '20px' }}>
-            {/* 대댓글 입력 */}
-            <input
-              type="text"
-              value={newComment}
-              onChange={handleInputChange}
-              placeholder="대댓글을 입력하세요"
-            />
-            <button onClick={() => handleAddComment(comment.id)}>대댓글</button>
-            <Comments comments={comment.replies} />
-          </div>
+          <button onClick={() => comment.likes++}>
+            좋아요 {comment.likes}
+          </button>
+          <button onClick={() => comment.dislikes++}>
+            싫어요 {comment.dislikes}
+          </button>
+          <br />
+
+          {/* 대댓글 입력창 */}
+          <input type="text" placeholder="대댓글을 입력하세요" />
+          <button>대댓글 추가</button>
+
+          {/* 대댓글 목록 */}
+          {comment.replies.map((reply) => (
+            <div key={reply.id}>
+              <p>{reply.content}</p>
+              <button onClick={() => reply.likes++}>
+                좋아요 {reply.likes}
+              </button>
+              <button onClick={() => reply.dislikes++}>
+                싫어요 {reply.dislikes}
+              </button>
+            </div>
+          ))}
         </div>
       ))}
-    </>
+    </CommentContainer>
   );
 };
+
+const CommentContainer = styled.div`
+  width: 80%;
+  margin: 0 auto;
+`;
+
+const NewCommentInput = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const NewCommentArea = styled.div`
+  display: flex;
+  align-items: center;
+  flex-direction: row-reverse;
+`;
 
 export default Comments;
--- web/src/pages/Community/Community.tsx
+++ web/src/pages/Community/Community.tsx
@@ -2,37 +2,12 @@ import React from 'react';
 import Comments from '../../components/Community_Post/Comments';
 
 const Community: React.FC = () => {
-  // 가상의 댓글 데이터
-  const commentsData = [
-    {
-      id: 1,
-      content: '첫 번째 댓글.',
-      replies: [
-        {
-          id: 11,
-          content: '첫 번째 댓글의 첫 번째 대댓글.',
-          replies: [],
-        },
-        {
-          id: 12,
-          content: '첫 번째 댓글의 두 번째 대댓글',
-          replies: [],
-        },
-      ],
-    },
-    {
-      id: 2,
-      content: '두 번째 댓글입니다.',
-      replies: [],
-    },
-  ];
-
   return (
     <div>
       <header>헤더</header>
       <div>
         Contents
-        <Comments comments={commentsData} />
+        <Comments />
       </div>
       <footer>푸터</footer>
     </div>
