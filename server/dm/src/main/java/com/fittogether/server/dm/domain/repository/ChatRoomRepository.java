4b84de59054b2f4eb40322ebd6ceb8ff2e643dc4
WI <sri07120@naver.com>
2023-07-30 15:00:46 +0900

    Revert "feat:[dm]- 메이트 요청"
    This reverts commit 5f4b11ee2b6191d55e97c2e7534f688773abd268.

--- server/dm/src/main/java/com/fittogether/server/dm/domain/repository/ChatRoomRepository.java
+++
@@ -1,9 +0,0 @@
-package com.fittogether.server.dm.domain.repository;
-
-import com.fittogether.server.dm.domain.entity.ChatRoom;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ChatRoomRepository extends JpaRepository<ChatRoom,Long> {
-
-
-}
