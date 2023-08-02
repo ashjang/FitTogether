1f3a3bd5c905c8eac593a31bf82162ec67b25626
YoonHyeJin <emfkdlvnem@gmail.com>
2023-08-02 16:27:33 +0900

    feat:[web/develop]-json파일추가

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -17,30 +17,29 @@ import Post from './pages/Post';
 import CreatePost from './pages/CreatePost';
 import MyPage from './pages/MyPage';
 import MyVideos from './pages/MyVideos';
-
 import './index.css';
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Header />
-      <Routes>
-        <Route path="/" />
-        <Route path="/signin" element={<SignIn />} />
-        <Route path="/signup" element={<SignUp />} />
-        <Route path="/messenger" element={<Messenger />} />
-        <Route path="/bookmark" element={<Bookmark />} />
-        <Route path="/exerciseinfo" element={<ExerciseInfo />} />
-        <Route path="/findmate" element={<FindMate />} />
-        <Route path="/community" element={<Community />} />
-        <Route path="/community/post" element={<Post />} />
-        <Route path="/community/createpost" element={<CreatePost />} />
-        <Route path="/mypage" element={<MyPage />} />
-        <Route path="/mypage/myvideos" element={<MyVideos />} />
-      </Routes>
-      <Footer />
-    </BrowserRouter>
-  );
+    return (
+        <BrowserRouter>
+            <Header />
+            <Routes>
+                <Route path="/" element={<MainPage />} />
+                <Route path="/signin" element={<SignIn />} />
+                <Route path="/signup" element={<SignUp />} />
+                <Route path="/messenger" element={<Messenger />} />
+                <Route path="/bookmark" element={<Bookmark />} />
+                <Route path="/exerciseinfo" element={<ExerciseInfo />} />
+                <Route path="/findmate" element={<FindMate />} />
+                <Route path="/community" element={<Community />} />
+                <Route path="/community/post" element={<Post />} />
+                <Route path="/community/createpost" element={<CreatePost />} />
+                <Route path="/mypage" element={<MyPage />} />
+                <Route path="/mypage/myvideos" element={<MyVideos />} />
+            </Routes>
+            <Footer />
+        </BrowserRouter>
+    );
 }
 
 export default App;
