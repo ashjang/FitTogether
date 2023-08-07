28788989efb909097bd561e1a2c30b8f998d10ab
DESKTOP-UPBTJS0\yangs <yangsh9161@gmail.com>
2023-08-07 23:14:30 +0900

    feat[recoil을 사용한 로그인 구현]

--- web/src/components/Sign/SignInSetting.tsx
+++ web/src/components/Sign/SignInSetting.tsx
@@ -1,15 +1,22 @@
 import React, { useState } from 'react';
+import { useRecoilState, useSetRecoilState } from 'recoil';
 import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 import { css } from '@emotion/react';
 import styled from '@emotion/styled';
 import KakaoTalk_logo from '../../assets/KakaoTalk_logo.png';
+import { loggedInState, signInState } from '../../recoil/AuthState/atoms';
 
-interface Props {}
+// interface Props {}
 
-const SignInSetting: React.FC<Props> = () => {
-    const [nickname, setnickName] = useState<string>('');
-    const [password, setPassword] = useState<string>('');
-    const [token, setToken] = useState<string>('');
+const SignInSetting: React.FC = () => {
+    // const [nickname, setnickName] = useState<string>('');
+    // const [password, setPassword] = useState<string>('');
+    // const [token, setToken] = useState<string>('');
+    const navigate = useNavigate();
+    const [signInData, setSignInData] = useRecoilState(signInState);
+    const setToken = useSetRecoilState(loggedInState);
+    const [errorMessage, setErrorMessage] = useState<string>('');
 
     const KAKAO_REST_API_KEY = `${import.meta.env.VITE_APP_KAKAO_REST_API_KEY}`;
     const REDIRECT_URI = `${import.meta.env.VITE_APP_REDIRECT_URI}`;
@@ -18,23 +25,79 @@ const SignInSetting: React.FC<Props> = () => {
     const handleKakaoLogin = () => {
         window.location.href = kakaoURL;
     };
+
+    // const handleSignIn = async (event: React.FormEvent) => {
+    //     event.preventDefault();
+
+    //     const formData = {
+    //         nickname: nickname,
+    //         password: password,
+    //     };
+
+    //     try {
+    //         const response = await axios.post<{ token: string }>('/users/signin', formData);
+
+    //         if (response.status === 200) {
+    //             setToken(response.data.token);
+    //             localStorage.setItem('token', token);
+    //             // 로그인 성공 처리
+    //         } else {
+    //             // 로그인 실패 처리
+    //         }
+    //     } catch (error) {
+    //         console.error('Error:', error);
+    //     }
+    // };
+
     const handleSignIn = async (event: React.FormEvent) => {
         event.preventDefault();
 
-        const formData = {
-            nickname: nickname,
-            password: password,
-        };
+        // try {
+        //     const response = await axios.post<{ token: string }>('/users/signin', signInData);
 
+        //     if (response.status === 200) {
+        //         const token = response.data.token;
+        //         setToken(token);
+        //         localStorage.setItem('token', token);
+
+        //         // 로그인 성공 처리: 메인페이지로 이동
+        //         navigate('/');
+        //     } else {
+        //         // 로그인 실패 처리
+        //         if (response.status === 401) {
+        //             // 인증 실패 (예: 아이디 또는 비밀번호 불일치)
+        //             console.log('로그인 실패: 아이디 또는 비밀번호가 잘못되었습니다.');
+        //         } else {
+        //             // 기타 다른 실패 상황 처리
+        //             console.log('로그인 실패: 서버 응답에 문제가 발생했습니다.');
+        //         }
+        //     }
+        // } catch (error) {
+        //     console.error('Error:', error);
+        // }
         try {
-            const response = await axios.post<{ token: string }>('/users/signin', formData);
+            // 서버 대신에 users.json에서 대조하는 로직
+            const response = await axios.get('/users.json');
+            const users = response.data.users; // users.json 파일에서 users 배열만 가져오도록 수정
 
-            if (response.status === 200) {
-                setToken(response.data.token);
+            const user = users.find(
+                (u: any) => u.nickname === signInData.nickname && u.password === signInData.password
+            );
+
+            if (user) {
+                const token = signInData.nickname; // 임시 토큰: 닉네임 사용
+                setToken(token);
                 localStorage.setItem('token', token);
-                // 로그인 성공 처리
+
+                // 로그인 성공 처리: 메인페이지로 이동
+                navigate('/');
             } else {
                 // 로그인 실패 처리
+                if (!users.some((u: any) => u.nickname === signInData.nickname)) {
+                    setErrorMessage('아이디가 존재하지 않습니다.');
+                } else {
+                    setErrorMessage('비밀번호가 일치하지 않습니다.');
+                }
             }
         } catch (error) {
             console.error('Error:', error);
@@ -53,22 +116,28 @@ const SignInSetting: React.FC<Props> = () => {
                     <InputField>
                         <input
                             type="text"
-                            value={nickname}
-                            onChange={(e) => setnickName(e.target.value)}
+                            value={signInData.nickname}
+                            onChange={(e) =>
+                                setSignInData({ ...signInData, nickname: e.target.value })
+                            }
                             placeholder="아이디(닉네임)를 입력하세요"
                         />
                     </InputField>
                     <InputField>
                         <input
                             type="password"
-                            value={password}
-                            onChange={(e) => setPassword(e.target.value)}
+                            value={signInData.password}
+                            onChange={(e) =>
+                                setSignInData({ ...signInData, password: e.target.value })
+                            }
                             placeholder="비밀번호를 입력하세요"
                         />
                     </InputField>
                     <SignInButton type="submit">로그인</SignInButton>
                 </FormContainer>
             </Container>
+            {errorMessage && <ErrorMessage>{errorMessage}</ErrorMessage>}{' '}
+            {/* 추가: 에러 메시지 표시 */}
         </Page>
     );
 };
@@ -78,7 +147,8 @@ const Page = styled.div`
     justify-content: center;
     align-items: center;
     // min-height는 삭제 예정
-    min-height: calc(100vh - 300px);
+    // min-height: calc(100vh - 300px);
+    flex-direction: column;
 `;
 
 const Container = styled.div`
@@ -150,6 +220,12 @@ const SignInButton = styled.button`
             background-color: #0056b3;
         }
     `}
+`;
+
+const ErrorMessage = styled.div`
+    color: #007bff;
+    font-weight: bold;
+    font-size: 14px;
 `;
 
 export default SignInSetting;
---
+++ web/src/recoil/AuthState/atoms.ts
@@ -0,0 +1,39 @@
+import { atom, useRecoilState } from 'recoil';
+
+// 초기값: 로그인이 되어 있지 않은 상태
+export const loggedInState = atom({
+    key: 'loggedInState',
+    default: '',
+});
+
+export const signInState = atom({
+    key: 'signInState',
+    default: {
+        nickname: '',
+        password: '',
+    },
+});
+
+// 로그인 토큰 설정
+export function useSetTokenState() {
+    const [, setToken] = useRecoilState(loggedInState);
+
+    // 로그인 상태를 업데이트하는 함수를 반환
+    return (token: string) => {
+        setToken(token);
+    };
+}
+
+// export const authState = atom({
+//     key: 'authState',
+//     default: {
+//         isLoggedIn: false,
+//         userId: null as number | null, // 아이디의 타입을 지정합니다
+//         // other user info...
+//     },
+// });
+
+// export const emailState = atom<string>({
+//     key: 'emailState',
+//     default: '', // 초기값은 비어있는 문자열로 설정하거나, 로그인 시에 로그인한 사용자의 이메일 값을 설정합니다.
+// });
