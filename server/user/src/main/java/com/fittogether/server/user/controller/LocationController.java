1d83cdcacc999b428ac6f088f23a7933873ae311
ashjang <vkdvkdgkfla@naver.com>
2023-08-07 23:23:44 +0900

    feat: [user] - 3km 내 사용자 조회
    Changes
    ---
    
    - 내 위치 기준 3km 내 사용자 조회 기능 추가

--- server/user/src/main/java/com/fittogether/server/user/controller/LocationController.java
+++ server/user/src/main/java/com/fittogether/server/user/controller/LocationController.java
@@ -1,10 +1,13 @@
 package com.fittogether.server.user.controller;
 
+import com.fittogether.server.user.domain.dto.AnotherUserDto;
 import com.fittogether.server.user.service.LocationService;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/location")
@@ -19,5 +22,11 @@ public class LocationController {
 
         locationService.updateMyLocation(token, latitude, longitude);
         return ResponseEntity.ok().body("나의 위치를 업데이트 완료");
+    }
+
+    @ApiOperation(value = "3km 내 사용자 조회")
+    @GetMapping("/map")
+    public ResponseEntity<List<AnotherUserDto>> getUserWithinBoundary(@RequestHeader(name = "X-AUTH-TOKEN") String token) {
+        return ResponseEntity.ok(locationService.getUsersWithin3km(token));
     }
 }
