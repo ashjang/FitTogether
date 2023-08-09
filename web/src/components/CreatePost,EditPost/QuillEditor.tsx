68d90e5723e820f376f48eae776d8c6f3f71540c
서동경 <ehhdrud@icloud.com>
2023-08-09 19:37:47 +0900

    feat: [feature/Community] - 게시판 에디터 이미지 핸들링 구현중

--- web/src/components/CreatePost,EditPost/QuillEditor.tsx
+++ web/src/components/CreatePost,EditPost/QuillEditor.tsx
@@ -1,11 +1,12 @@
-import { useRef, useMemo } from 'react';
+import { useRef } from 'react';
+// import { useMemo, useEffect } from 'react';
 import ReactQuill from 'react-quill';
 import styled from '@emotion/styled';
 import 'react-quill/dist/quill.snow.css';
 import { useRecoilState } from 'recoil';
 import { titleState, descriptionState } from '../../recoil/posts/atoms';
 
-export const formats = [
+const formats = [
     'header',
     'font',
     'size',
@@ -26,27 +27,61 @@ export const formats = [
     'width',
 ];
 
+const modules = {
+    toolbar: {
+        container: [
+            ['link', 'image', 'video'],
+            [{ header: [1, 2, 3, false] }],
+            ['bold', 'italic', 'underline', 'strike'],
+            ['blockquote'],
+            [{ list: 'ordered' }, { list: 'bullet' }],
+            [{ color: [] }, { background: [] }],
+            [{ align: [] }],
+        ],
+    },
+};
+
 const QuillEditor = () => {
-    const QuillRef = useRef<ReactQuill>();
     const [title, setTitle] = useRecoilState(titleState);
     const [description, setDescription] = useRecoilState(descriptionState);
+    const quillRef = useRef<ReactQuill>(null);
+    console.log(description);
 
-    const modules = useMemo(
-        () => ({
-            toolbar: {
-                container: [
-                    ['link', 'image', 'video'],
-                    [{ header: [1, 2, 3, false] }],
-                    ['bold', 'italic', 'underline', 'strike'],
-                    ['blockquote'],
-                    [{ list: 'ordered' }, { list: 'bullet' }],
-                    [{ color: [] }, { background: [] }],
-                    [{ align: [] }],
-                ],
-            },
-        }),
-        []
-    );
+    // useEffect(() => {
+    //     const handleImage = () => {
+    //         const input = document.createElement('input');
+    //         input.setAttribute('type', 'file');
+    //         input.setAttribute('accept', 'image/*');
+    //         input.click();
+    //         input.onchange = async () => {
+    //             if (!input.files || !quillRef.current) return;
+    //             const file = input.files[0];
+
+    //             const { getEditor } = quillRef.current;
+    //             const range = getEditor().getSelection(true);
+
+    //             try {
+    //                 // 서버에 업로드 한뒤 이미지 태그에 삽입할 url을 반환받도록 구현
+    //                 const filePath = `contents/temp/${Date.now()}`;
+    //                 const url = await uploadImage(file, filePath);
+
+    //                 // 받아온 url을 이미지 태그에 삽입
+    //                 quillRef.current.getEditor().insertEmbed(range.index, 'image', url);
+
+    //                 // 사용자 편의를 위해 커서 이미지 오른쪽으로 이동
+    //                 // quillRef.current.getEditor().setSelection(range.index + 1);
+    //             } catch (error) {
+    //                 console.log(error);
+    //             }
+    //         };
+    //     };
+
+    //     if (quillRef.current) {
+    //         const { getEditor } = quillRef.current;
+    //         const toolbar = getEditor().getModule('toolbar');
+    //         toolbar.addHandler('image', handleImage);
+    //     }
+    // }, []);
 
     return (
         <EditorComponent>
@@ -57,17 +92,13 @@ const QuillEditor = () => {
                 placeholder=" title"
             />
             <ReactQuillComponent
-                ref={(element) => {
-                    if (element !== null) {
-                        QuillRef.current = element;
-                    }
-                }}
+                ref={quillRef}
+                placeholder="contents..."
                 value={description}
-                onChange={setDescription}
+                theme="snow"
                 modules={modules}
                 formats={formats}
-                theme="snow"
-                placeholder="contents..."
+                onChange={setDescription}
             />
         </EditorComponent>
     );
