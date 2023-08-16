7203ebc007e4b6e6d1e102443d42011c094e55be
DESKTOP-UPBTJS0\yangs <yangsh9161@gmail.com>
2023-08-16 14:44:43 +0900

    feat[Bookmark 수정]

--- web/src/pages/MyVideos.tsx
+++ web/src/pages/MyVideos.tsx
@@ -1,46 +1,69 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import styled from '@emotion/styled';
+import { css } from '@emotion/react';
+import { FaPlus } from 'react-icons/fa';
 import MyVideoList from '../components/MyVideos/MyVideoList';
 
 const MyVideos: React.FC = () => {
-
-  return (
-    <>
-      <MyVideosContainer>
-        <ListTitle>맘에들어 등산</ListTitle>
-      </MyVideosContainer>
-      <MyVideoContent>
-        <MyVideoList />
-      </MyVideoContent>
-    </>
-  );
+    return (
+        <>
+            <MyVideosContainer>
+                <ListTitle>
+                    <TitleTextStyle>맘에들어 등산</TitleTextStyle>
+                    <Link to="/exerciseInfo">
+                        <FaPlus css={[rightAlignedStyle, icon]} />
+                    </Link>
+                </ListTitle>
+            </MyVideosContainer>
+            <MyVideoContent>
+                <MyVideoList />
+            </MyVideoContent>
+        </>
+    );
 };
 
 const MyVideosContainer = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: center;
+    display: flex;
+    align-items: center;
+    justify-content: center;
 `;
 
 const ListTitle = styled.div`
-  width: 1200px;
-  border-bottom: 1px solid black;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  padding: 8px;
-  padding-bottom: 30px;
-  margin-top: 70px;
-  margin-bottom: 70px;
-  font-size: 3rem;
-  font-weight: bold;
+    width: 1200px;
+    border-bottom: 1px solid black;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: 8px;
+    padding-bottom: 30px;
+    margin-top: 70px;
+    margin-bottom: 70px;
+    // font-size: 3rem;
+    // font-weight: bold;
+`;
+
+const TitleTextStyle = styled.div`
+    flex: 25;
+    text-align: center;
+    font-size: 3rem;
+`;
+
+const rightAlignedStyle = css`
+    flex: 1;
+    text-align: right;
+    // font-size: 16px;
+`;
+
+const icon = css`
+    cursor: pointer;
 `;
 
 const MyVideoContent = styled.div`
-  display: flex;
-  align-items: center;
-  flex-direction: column;
+    display: flex;
+    align-items: center;
+    flex-direction: column;
 `;
 
 export default MyVideos;
