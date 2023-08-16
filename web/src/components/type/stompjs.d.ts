f418a0b017ca56a8afd60bbb6af0c79f172e7988
YoonHyeJin <emfkdlvnem@gmail.com>
2023-08-16 22:59:59 +0900

    feat:[feature/Messenger]채팅 구조 조정

--- web/src/components/type/stompjs.d.ts
+++ web/src/components/type/stompjs.d.ts
@@ -1,11 +1,39 @@
+// stomp.d.ts
 declare module 'stompjs' {
     namespace Stomp {
         interface Client {
-            // Define Stomp Client methods and properties here
+            connected: boolean;
+            connect(headers: any, connectCallback: (frame: any) => void): void;
+            disconnect(disconnectCallback?: () => void): void;
+            subscribe(
+                destination: string,
+                callback: (message: any) => void,
+                headers?: any
+            ): Subscription;
+            send(destination: string, headers?: any, body?: string): void;
+        }
+
+        interface Subscription {
+            id: string;
+            unsubscribe(): void;
         }
 
         function over(ws: WebSocket): Client;
     }
 
+    const Stomp: {
+        over: (ws: any) => Stomp.Client;
+    };
+
     export = Stomp;
 }
+
+// websocket.ts
+// import SockJS from ‘sockjs-client’;
+// import Stomp from ‘stompjs’;
+
+// const socket = new SockJS(‘http://localhost:8080/ws’);
+
+// const stompClient: Stomp.Client = Stomp.over(socket);
+
+// export { socket, stompClient };
