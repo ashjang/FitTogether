d38b67be01969778aab05f2c73e2e2be08ae42b5
ashjang <vkdvkdgkfla@naver.com>
2023-08-21 18:00:13 +0900

    fix: [user] - 실행 에러
    Changes
    ---
    
    - FeignClient 설정

--- server/src/main/java/com/fittogether/server/FitTogetherApplication.java
+++ server/src/main/java/com/fittogether/server/FitTogetherApplication.java
@@ -3,6 +3,7 @@ package com.fittogether.server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -25,6 +26,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableCaching
 @EnableJpaAuditing
 @EnableScheduling
+@EnableFeignClients
 public class FitTogetherApplication {
     public static void main(String[] args) {
         SpringApplication.run(FitTogetherApplication.class, args);
